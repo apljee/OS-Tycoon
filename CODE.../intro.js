3336baa3a4c128cc31a31e49d2c9e63b67414dbe
apljee <apljee23@hotmail.com>
2013-12-15 18:48:14 -0500

    Update intro.js

--- CODE.../intro.js
+++ CODE.../intro.js
@@ -1,5 +1,4 @@
 //* This is just a file to put in the code folder until we have some code here that we will:
-A: Not be using in the game
-B: Use for a later date
-C: Test code
-*//
+//* A: Not be using in the game
+//* B: Use for a later date
+//* C: Test code
